2b91adf6a93326e29690a33b6c72ef318731735a
Your Name <you@example.com>
2022-01-31 13:42:27 -0300

    add implementation awsBasicCredentials

--- awsv2/src/main/java/com/example/awssdk/service/S3Service.java
+++ awsv2/src/main/java/com/example/awssdk/service/S3Service.java
@@ -1,6 +1,8 @@
 package com.example.awssdk.service;
 
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
@@ -13,12 +15,19 @@ public class S3Service {
 
     public List<String> getList() {
 
+        // If you need to set credentials manually (e.g from vault)
+        // AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
+        //        "your_access_key_id",
+        //        "your_secret_access_key");
+
         S3Client s3 = S3Client.builder()
+                //.credentialsProvider(StaticCredentialsProvider.create(awsCreds))
                 .region(Region.US_EAST_1)
                 .build();
 
         ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
         ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
+
 
         List<String> list = new ArrayList<>();
 
